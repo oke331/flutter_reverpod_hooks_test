b0a46d3efada3486fc64af0b9fbfbc99b544a0dd
oke331 <takeo.takeo331@gmail.com>
2021-01-31 21:54:55 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_riverpod_hooks_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_riverpod_hooks_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+final counterProvider = StateNotifierProvider((_) => Counter());
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+  void increment() => state++;
+}
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: CounterApp(),
+    ),
+  );
+}
+
+class CounterApp extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final state = useProvider(counterProvider.state);
+    final counter = useProvider(counterProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Counter/app'),
+        ),
+        body: Center(
+          child: Text(state.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => counter.increment(),
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
